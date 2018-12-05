6ca78d890131956f559d2e195cc58060c0f55c75
Long <1412299@student.hcmus.edu.vn>
2018-12-05 21:20:47 +0700

    Fixing console.log err es.lint

--- src/server/api/resources/Product/product.controller.js
+++ src/server/api/resources/Product/product.controller.js
@@ -7,20 +7,21 @@ export const getAll = (req, res) => {
 export const getOne = (req, res) => {
   const productId = req.params.id;
   Product.findById(productId, (err, product) => {
-    if (err) return console.error(err);
+    if (err) return Error(err);
     return res.status(200).json(product);
   });
 };
 
 export const createOne = (req, res) => {
-  const newProduct = {};
-  newProduct.name = req.body.name;
-  newProduct.price = req.body.price;
-  newProduct.category = req.body.category;
-  newProduct.subCategoty = req.body.subCategoty;
-  newProduct.description = req.body.description;
-  newProduct.active = req.body.active;
+  const newProduct = req.body;
   newProduct.image = req.file.url;
+  // newProduct.name = req.body.name;
+  // newProduct.price = req.body.price;
+  // newProduct.category = req.body.category;
+  // newProduct.subCategoty = req.body.subCategoty;
+  // newProduct.description = req.body.description;
+  // newProduct.active = req.body.active;
+  // newProduct.image = req.file.url;
   const product = new Product(newProduct);
   product
     .save()
@@ -28,6 +29,6 @@ export const createOne = (req, res) => {
       res.json(data);
     })
     .catch((err) => {
-      console.log(err);
+      Error(err);
     });
 };
